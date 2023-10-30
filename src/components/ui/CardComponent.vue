12658a8254f4d072b98873840bf72d5c1feab153
Destcable <artmobpavlov21@gmail.com>
2023-10-31 00:34:18 +0300

    [upd]

--- src/components/ui/CardComponent.vue
+++ src/components/ui/CardComponent.vue
@@ -5,13 +5,14 @@ export default{
 </script>
 
 <template>
-    <div class="card text-center" style="width: 18rem;">
-        <img  class="card-img-top" alt="...">
+    <div class="card text-center" style="width: 30rem;">
+        <img  class="card-img-top">
         <div class="card-body">
-            <p class="fw-light">ДОМАШНИЕ РАСТЕНИЯ</p>
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link text-decoration-none">ЧИТАТЬ</a>
+            <p class="text-danger">ДОМАШНИЕ РАСТЕНИЯ</p>
+            <h5 class="card-title">Комнатные Растения — Целители. Для Чего Нужно Разводить Домашние Растения</h5>
+            <p class=" text-secondary">31.10.2023</p>
+            <p class="card-text">Одни называют комнатные растения пылесборниками, другие возводят их в ранг природных лекарей. Очевидно одно: растения дают силы и заряжают энергией, только будьте очень разборчивы в выборе зелёных друзей. Большинство людей заводят комнатные растения для красоты.</p>
+            <a href="#" class="card-link text-decoration-none text-danger">ЧИТАТЬ</a>
         </div>
     </div>
 </template>
