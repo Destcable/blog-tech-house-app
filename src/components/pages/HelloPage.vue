8726fec993d317162987873950a10796c61fd9d8
Destcable <artmobpavlov21@gmail.com>
2023-10-31 00:24:17 +0300

    [upd]

---
+++ src/components/pages/HelloPage.vue
@@ -0,0 +1,23 @@
+<script>
+import HeaderComponent from "../ui/HeaderComponent.vue";
+import CardComponent from "../ui/CardComponent.vue";
+export default {
+  name: 'HelloPage',
+  components: {
+    HeaderComponent,
+    CardComponent
+},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+  <HeaderComponent />
+  <div class="w-100 d-flex justify-content-center">
+    <CardComponent />
+  </div>
+</template>
+
+<style scoped></style>
