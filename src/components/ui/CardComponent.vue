883c8a1d52924dbf743e58d7399c821a37a68abe
Destcable <artmobpavlov21@gmail.com>
2023-10-31 00:27:28 +0300

    [upd]

--- src/components/ui/CardComponent.vue
+++ src/components/ui/CardComponent.vue
@@ -5,14 +5,15 @@ export default{
 </script>
 
 <template>
-    <div class="card" style="width: 18rem;">
+    <div class="card text-center" style="width: 18rem;">
         <img  class="card-img-top" alt="...">
         <div class="card-body">
+            <p class="fw-light">ДОМАШНИЕ РАСТЕНИЯ</p>
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Ч И Т А Т Ь</a>
+            <a href="#" class="card-link text-decoration-none">ЧИТАТЬ</a>
         </div>
-      </div>
+    </div>
 </template>
 
 <style scoped>
