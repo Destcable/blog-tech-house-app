a2401b757905e831b769c8701d6070cd0732c4d6
Destcable <artmobpavlov21@gmail.com>
2023-11-01 17:11:29 +0300

    [upd] CategoriesComponent

--- src/components/pages/HelloPage.vue
+++ src/components/pages/HelloPage.vue
@@ -1,11 +1,13 @@
 <script>
 import HeaderComponent from "../ui/HeaderComponent.vue";
 import CardComponent from "../ui/CardComponent.vue";
+import CategoriesComponent from "../ui/CategoriesComponent.vue";
 export default {
   name: 'HelloPage',
   components: {
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    CategoriesComponent
   },
   props: {
     msg: String
@@ -26,30 +28,19 @@ export default {
   <HeaderComponent />
   <div class="container">
     <div class="row justify-content-center">
+      
       <div class="col">
         <div v-for="(item, index) in items" :key="index" class="">
             <CardComponent :category_name="item.category_name" />
         </div>
       </div>
+
       <div class="col">
-        <div class="border border-secondary rounden p-3">
-          <div class="border border-danger p-2 rounded text-center ">
-            РУБРИКИ
-          </div>
-        </div>
+          <CategoriesComponent />
       </div>
+
     </div>
   </div>
-  <!-- <div v-for="(item, index) in items" :key="index" class="">
-    <div class="w-100 mt-5 d-flex justify-content-center">
-      <CardComponent :category_name="item.category_name" />
-    </div>
-    <div>
-      <div class="">
-        1
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <style scoped></style>
---
+++ src/components/ui/CategoriesComponent.vue
@@ -0,0 +1,13 @@
+<script> 
+export default {
+  name: 'CategoriesComponent',
+}
+</script>
+
+<template>
+    <div class="shadow rounded p-3">
+        <div class="border border-danger p-2 rounded text-center">
+            РУБРИКИ
+        </div>
+    </div>
+</template>
