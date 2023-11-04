d02c170062078527d9368dc3c04dcfe38f494081
Destcable <artmobpavlov21@gmail.com>
2023-11-05 00:02:16 +0300

    [update]

--- src/components/pages/HelloPage.vue
+++ src/components/pages/HelloPage.vue
@@ -15,9 +15,9 @@ export default {
   data() {
     return {
       items: [
-        { id: 1, category_name: "РАСТЕНИЯ" },
-        { id: 2, category_name: "СТОЛЫ" },
-        { id: 3, category_name: "СТУЛЬЯ" },
+        { id: 1, category_name: "РАСТЕНИЯ", title: "title1", created_at: "31.10.2023", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam, purus nec egestas ullamcorper, dolor risus faucibus sem, sit amet venenatis ante mauris eu dui. Aliquam sollicitudin dui sed tincidunt commodo" },
+        { id: 2, category_name: "СТОЛЫ", title: "title2", created_at: "03.11.2023", text: "Vestibulum tempus pretium orci, et eleifend elit viverra eu. Vestibulum aliquet, lectus vel ultrices consequat, quam leo cursus nisi, vitae dictum purus purus eget libero. Nam mollis pellentesque purus non sagittis. Nullam quis gravida mauris" },
+        { id: 3, category_name: "СТУЛЬЯ", title: "title3", created_at: "04.11.2023", text: "hasellus nulla augue, dictum vel faucibus faucibus, pharetra nec dui. Cras et fermentum enim. Praesent vehicula lacus non libero volutpat, sed sodales mauris iaculis. Ut elit magna, ornare non pellentesque eu, auctor ut orci. Aliquam vitae fermentum urna" },
       ]
     }
   }
@@ -31,7 +31,12 @@ export default {
       <div class="d-flex justify-content-center row">
         <div class="col-md-8 w-50">
           <div v-for="(item, index) in items" :key="index">
-            <CardComponent :category_name="'ДОМАШНИЕ ' + item.category_name" />
+            <CardComponent 
+              :title="item.title"
+              :category_name="'ДОМАШНИЕ ' + item.category_name"
+              :created_at="item.created_at"
+              :text="item.text"
+            />
           </div>
         </div>
         <div class="col-md-3">
--- src/components/ui/CardComponent.vue
+++ src/components/ui/CardComponent.vue
@@ -3,6 +3,9 @@ export default{
     name: 'CardComponent',
     props: {
         category_name: String,
+        title: String,
+        created_at: String,
+        text: String,
     }
 }
 </script>
@@ -12,9 +15,9 @@ export default{
         <img  class="card-img-top">
         <div class="card-body">
             <p class="text-danger">{{ category_name }}</p>
-            <h5 class="card-title">Комнатные Растения — Целители. Для Чего Нужно Разводить Домашние Растения</h5>
-            <p class=" text-secondary">31.10.2023</p>
-            <p class="card-text">Одни называют комнатные растения пылесборниками, другие возводят их в ранг природных лекарей. Очевидно одно: растения дают силы и заряжают энергией, только будьте очень разборчивы в выборе зелёных друзей. Большинство людей заводят комнатные растения для красоты.</p>
+            <h5 class="card-title">{{ title }}</h5>
+            <p class=" text-secondary">{{ created_at }}</p>
+            <p class="card-text">{{ text }}</p>
             <a href="#" class="card-link text-decoration-none text-danger">ЧИТАТЬ</a>
         </div>
     </div>
