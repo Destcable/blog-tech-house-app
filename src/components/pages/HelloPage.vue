b3fdd2413f314ff422835785b09f21c9f39e1ca7
Destcable <artmobpavlov21@gmail.com>
2023-11-06 22:43:31 +0300

    [upd]

--- src/components/pages/HelloPage.vue
+++ src/components/pages/HelloPage.vue
@@ -1,9 +1,11 @@
 <script>
 import getPosts from "../api/getPosts";
 import transformResponsePosts from "../utils/transformResponse/transformResponsePosts"
+import sortByDateDescending from "../utils/sortByDateDescending"
 import HeaderComponent from "../ui/HeaderComponent.vue";
 import CardComponent from "../ui/CardComponent.vue";
 import CategoriesComponent from "../ui/CategoriesComponent.vue";
+
 export default {
   name: 'HelloPage',
   components: {
@@ -21,7 +23,11 @@ export default {
   },
   mounted() {
     getPosts()
-      .then(response => this.items = transformResponsePosts(response.data) );
+      .then(response =>{ 
+        this.items = sortByDateDescending(
+          transformResponsePosts(response.data), 'created_at'
+        )
+      });
   }
 }
 </script>
@@ -33,7 +39,10 @@ export default {
       <div class="d-flex justify-content-center row">
         <div class="col-md-8 w-50">
           <div v-for="(item, index) in items" :key="index">
-            <CardComponent :title="item.title" category_name="ДОМАШНИЕ РАСТЕНИЯ" :created_at="item.created_at"
+            <CardComponent 
+              :title="item.title" 
+              category_name="ДОМАШНИЕ РАСТЕНИЯ" 
+              :created_at="item.created_at"
               :text="item.body" />
           </div>
         </div>
