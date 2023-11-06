16fbc317016ac9bd0e8d9a91b6951213071a3830
Destcable <artmobpavlov21@gmail.com>
2023-11-06 15:17:05 +0300

    [update]

--- src/components/api/getPosts.js
+++ src/components/api/getPosts.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default function getPosts() {
-     
+    return axios.get('http://127.0.0.1:8000/api/v1/posts');
 }
--- src/components/pages/HelloPage.vue
+++ src/components/pages/HelloPage.vue
@@ -1,4 +1,5 @@
 <script>
+import getPosts from "../api/getPosts";
 import HeaderComponent from "../ui/HeaderComponent.vue";
 import CardComponent from "../ui/CardComponent.vue";
 import CategoriesComponent from "../ui/CategoriesComponent.vue";
@@ -14,13 +15,13 @@ export default {
   },
   data() {
     return {
-      items: [
-        { id: 1, category_name: "РАСТЕНИЯ", title: "title1", created_at: "31.10.2023", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam, purus nec egestas ullamcorper, dolor risus faucibus sem, sit amet venenatis ante mauris eu dui. Aliquam sollicitudin dui sed tincidunt commodo" },
-        { id: 2, category_name: "СТОЛЫ", title: "title2", created_at: "03.11.2023", text: "Vestibulum tempus pretium orci, et eleifend elit viverra eu. Vestibulum aliquet, lectus vel ultrices consequat, quam leo cursus nisi, vitae dictum purus purus eget libero. Nam mollis pellentesque purus non sagittis. Nullam quis gravida mauris" },
-        { id: 3, category_name: "СТУЛЬЯ", title: "title3", created_at: "04.11.2023", text: "hasellus nulla augue, dictum vel faucibus faucibus, pharetra nec dui. Cras et fermentum enim. Praesent vehicula lacus non libero volutpat, sed sodales mauris iaculis. Ut elit magna, ornare non pellentesque eu, auctor ut orci. Aliquam vitae fermentum urna" },
-      ]
+      items: null,
     }
   },
+  mounted() {
+    getPosts()
+      .then(response => { this.items = response.data });
+  }
 }
 </script>
 
@@ -33,9 +34,9 @@ export default {
           <div v-for="(item, index) in items" :key="index">
             <CardComponent 
               :title="item.title"
-              :category_name="'ДОМАШНИЕ ' + item.category_name"
+              category_name="ДОМАШНИЕ РАСТЕНИЯ"
               :created_at="item.created_at"
-              :text="item.text"
+              :text="item.body"
             />
           </div>
         </div>
