33c78bad9cf7aa4d3ffdc0525452dd7c26170f0c
Destcable <artmobpavlov21@gmail.com>
2023-11-06 15:39:54 +0300

    [update]

--- src/components/utils/formatDate/formatDateToString.js
+++ src/components/utils/formatDate/formatDateToString.js
@@ -1,7 +1,7 @@
 export default function formatDateToString(inputDate) { 
     const date = new Date(inputDate);
     const day =  date.getUTCDate();
-    const month = date.getUTCMonth();
+    const month = date.getUTCMonth() + 1;
     const year = date.getUTCFullYear();
     return (day + '.' + month + '.' + year);
 }
