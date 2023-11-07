d4e79ae9a505c8d5f6b28c2991195f974585e40a
Destcable <artmobpavlov21@gmail.com>
2023-11-07 15:52:03 +0300

    [update]

--- native-app/App.js
+++ native-app/App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <Layout>
       <Card 
-        title="title1"  
+        title="title1"
+        created_at="18.07.2023"
       />
     </Layout>
   );
--- native-app/components/ui/Card/Card.js
+++ native-app/components/ui/Card/Card.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { MAIN_COLORS } from '../../../config/color';
 
-export default function Card({title}) {
+export default function Card({title, created_at}) {
     return (
         <View style={styles.card}>
             <Text style={styles.txt_category}>1213</Text>
             <Text>{title}</Text>
+            <Text>{created_at}</Text>
         </View>
     )
 }
